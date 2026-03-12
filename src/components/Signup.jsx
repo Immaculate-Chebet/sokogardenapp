c0de3698ce1f599d8bec395a075a9d3f69751718
IMMACULATE-CHEBET <clate605@gmail.com>
2026-03-12 14:06:42 +0300

    new update on sign up

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 const Signup = () => {
@@ -6,11 +7,55 @@ const Signup = () => {
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     const [tel,setTel]=useState('');
+    //define the three states an application will move to
+  const [loading,setLoading]=useState('');
+  const [success,setSuccess]=useState('');
+  const [error,setError]=useState('');
+
+  //below is a function that will handle the submit action
+  const handleSubmit=async(e)=>{
+    //below we prevent our site from reloading
+    e.preventDefault()
+
+    //update our loading hook with a message that will be displayed to the uses who are trying to register
+    setLoading('Please wait as registration is in progress...')
+    try{
+      //create a form data object that will enable you to capture the four details entered on the form
+      const formdata= new FormData();
+      //insert the four details in terms key=value pair
+      formdata.append('username',username);
+      formdata.append('email',email);
+      formdata.append('password',password);
+      formdata.append('phone',tel);
+      //by use of axios ,we can access the method post
+      const response = await axios.post('https://clate.alwaysdata.net/api/signup',formdata)
+      //set back loading to default
+      setLoading('');
+
+      //if everything goes well update the success hook with a message
+      setSuccess(response.data.message)
+      //clear your hooks
+      setUsername('');
+      setEmail('');
+      setPassword('');
+      setTel('');
+    }
+  catch(error){
+    //set the loading hook back to default
+    setLoading('');
+
+    //update the error hook with the message given back from the response
+    setError(error.message)
+  }
+  }
   return (
-    <div className='row justified-content-center mt-4'>
+    <div className='row justify-content-center mt-4'>
         <div className="card col-md-6 shadow p-4">
              <h1>Signup</h1>
-             <form>
+             <h5 className="text-warning">{loading}</h5>
+             <h3 className="text-success">{success}</h3>
+             <h4 className="text-danger">{error}</h4>
+             <form onSubmit={handleSubmit}>
                <input type="text" 
                placeholder='Enter your username' 
                className='form-control'
@@ -47,7 +92,7 @@ const Signup = () => {
                required /> <br />
 
                {/* {tel} */}
-               <input type="button" value='Signup' className='btn btn-primary' /> <br /> <br />
+               <input type="submit" value='Signup' className='btn btn-primary' /> <br /> <br />
                 Already had an account <Link to={"/signin"}>Sign in</Link>
 
 
